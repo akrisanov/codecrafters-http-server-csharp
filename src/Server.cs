e009cf6f8821d9a2043aa512d36c9b160a0894a2
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-03-04 10:33:10 +0300

    Respond with 200

--- src/Server.cs
+++ src/Server.cs
@@ -1,8 +1,21 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 Console.WriteLine("Starting server...");
 
 TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for a client
+var client = server.AcceptSocket(); // wait for a client to connect
+
+// create HTTP response
+string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+// send response to the client
+client.Send(responseBytes);
+
+client.Close();
+server.Stop();
+
+Console.WriteLine("Server stopped.");
