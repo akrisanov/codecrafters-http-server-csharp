c6c3e9a605a349e1861ac581e42073fb77758f2b
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-03-06 21:42:55 +0300

    Reusing a socket connection for client requests

--- src/Server.cs
+++ src/Server.cs
@@ -9,7 +9,7 @@ server.Start();
 
 while (true)
 {
-    // handle each client in a separate task, fire and forget 
+    // handle each client in a separate task, fire and forget
     _ = HandleClientConnectionAsync();
 }
 
@@ -17,37 +17,42 @@ async Task HandleClientConnectionAsync()
 {
     // wait for a client to connect
     using var client = await server.AcceptSocketAsync(); // Socket is unmanaged resource
-    
-    // receive request from the client
-    var requestBytes = new byte[1024];
-    var bytesRead = await client.ReceiveAsync(requestBytes);
-    var request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
-    
-    var requestMembers = request.Split("\r\n");
 
-    // parse the request
-    var requestTarget = requestMembers[0].Split(' ')[1]; // URL
-    var urlPath = requestTarget.TrimStart('/').Split('/');
-    var requestPath = urlPath[0]; // endpoint or page
-
-    // parse headers
-    var userAgentHeader = requestMembers.FirstOrDefault(p => p.StartsWith("User-Agent:"))?.Split(": ")[1] ?? "";
-
-    var (status, content) = requestPath switch
+    while (client.Connected)
     {
-        "" => ("200 OK", ""),
-        // return URL parameter as the response content
-        "echo" => ("200 OK", urlPath[1]),
-        // return User-Agent header as the response content
-        "user-agent" => ("200 OK", userAgentHeader),
-        // otherwise, return 404 Not Found
-        _ => ("404 Not Found", "")
-    };
+        // receive request from the client
+        var requestBytes = new byte[1024];
+        var bytesRead = await client.ReceiveAsync(requestBytes);
+        if (bytesRead == 0) break; // closed connection
 
-    // create HTTP response
-    var responseBytes = Encoding.UTF8.GetBytes(
-        $"HTTP/1.1 {status}\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}");
+        var request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
 
-    // send response to the client
-    await client.SendAsync(responseBytes);
+        var requestMembers = request.Split("\r\n");
+
+        // parse the request
+        var requestTarget = requestMembers[0].Split(' ')[1]; // URL
+        var urlPath = requestTarget.TrimStart('/').Split('/');
+        var requestPath = urlPath[0]; // endpoint or page
+
+        // parse headers
+        var userAgentHeader = requestMembers.FirstOrDefault(p => p.StartsWith("User-Agent:"))?.Split(": ")[1] ?? "";
+
+        var (status, content) = requestPath switch
+        {
+            "" => ("200 OK", ""),
+            // return URL parameter as the response content
+            "echo" => ("200 OK", urlPath[1]),
+            // return User-Agent header as the response content
+            "user-agent" => ("200 OK", userAgentHeader),
+            // otherwise, return 404 Not Found
+            _ => ("404 Not Found", "")
+        };
+
+        // create HTTP response
+        var responseBytes = Encoding.UTF8.GetBytes(
+            $"HTTP/1.1 {status}\r\nConnection: keep-alive\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}");
+
+        // send response to the client
+        await client.SendAsync(responseBytes);
+    }
 }
