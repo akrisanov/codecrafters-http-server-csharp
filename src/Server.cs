14ea2a975aeba35d2af2486e23289072853eab01
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-03-10 10:14:55 +0300

    Read request body

--- src/Server.cs
+++ src/Server.cs
@@ -5,10 +5,14 @@ using System.Text;
 Dictionary<int, string> httpStatusCodes = new()
 {
     [200] = "200 OK",
+    [201] = "201 Created",
     [404] = "404 Not Found",
 };
 
+var filesDir = GetFilesDir(args);
+
 Console.WriteLine("Starting server...");
+Console.WriteLine($"Serving files from {filesDir}");
 Console.WriteLine("Press Ctrl+C to stop the server");
 
 using TcpListener server = new(IPAddress.Any, 4221); // TcpListener is unmanaged resource
@@ -37,12 +41,11 @@ async Task HandleClientConnectionAsync()
         var requestMembers = request.Split("\r\n");
 
         // parse the request
-        var requestTarget = requestMembers[0].Split(' ')[1]; // URL
-        var urlPath = requestTarget.TrimStart('/').Split('/');
-        var requestPath = urlPath[0]; // endpoint or page
-
-        // parse headers
-        var userAgentHeader = requestMembers.FirstOrDefault(p => p.StartsWith("User-Agent:"))?.Split(": ")[1] ?? "";
+        var requestTarget = requestMembers[0].Split(' ');
+        var requestMethod = requestTarget[0]; // HTTP method
+        var requestUrl = requestTarget[1]; // URL
+        var urlPath = requestUrl.TrimStart('/').Split('/');
+        var endpoint = urlPath[0]; // endpoint or page
 
         // build a response
         var status = httpStatusCodes[200];
@@ -54,7 +57,7 @@ async Task HandleClientConnectionAsync()
         };
         var content = "";
 
-        switch (requestPath)
+        switch (endpoint)
         {
             case "": // root URL
                 break;
@@ -63,14 +66,30 @@ async Task HandleClientConnectionAsync()
                 headers["Content-Length"] = content.Length;
                 break;
             case "user-agent": // return User-Agent header as the response content
-                content = userAgentHeader;
+                var userAgentHeader = requestMembers.FirstOrDefault(p => p.StartsWith("User-Agent"));
+                var userAgent = userAgentHeader?.Split(": ")[1] ?? "";
+                content = userAgent;
                 headers["Content-Length"] = content.Length;
                 break;
             case "files": // serve files from the specified directory --directory
-                (status, var fileContent) = await ServeFileAsync(args[1], urlPath[1]);
-                headers["Content-Type"] = "application/octet-stream";
-                headers["Content-Length"] = fileContent.Length;
-                content = Encoding.UTF8.GetString(fileContent);
+                if (requestMethod == "GET")
+                {
+                    (status, var fileContent) = await ServeFileAsync(filesDir, urlPath[1]);
+                    headers["Content-Type"] = "application/octet-stream";
+                    headers["Content-Length"] = fileContent.Length;
+                    content = Encoding.UTF8.GetString(fileContent);
+                }
+                else if (requestMethod == "POST")
+                {
+                    var contentLengthHeader = requestMembers.FirstOrDefault(p => p.StartsWith("Content-Length"));
+                    var contentLength = int.TryParse(contentLengthHeader?.Split(": ")[1], out int result) ? result : 0;
+                    var requestBody = requestMembers[^1];
+                    status = await CreateFileAsync(filesDir, urlPath[1], requestBody);
+                }
+                else
+                {
+                    status = httpStatusCodes[404];
+                }
                 break;
             default:
                 status = httpStatusCodes[404];
@@ -93,13 +112,34 @@ async Task HandleClientConnectionAsync()
     }
 }
 
-async Task<(string status, byte[] content)> ServeFileAsync(string fileDir, string fileName)
+async Task<(string status, byte[] content)> ServeFileAsync(string dirName, string fileName)
 {
-    var filePath = Path.Combine(fileDir, fileName);
+    var filePath = Path.Combine(dirName, fileName);
+    if (!File.Exists(filePath)) return (httpStatusCodes[404], []);
+    var fileContent = await File.ReadAllBytesAsync(filePath);
+    return (httpStatusCodes[200], fileContent);
+}
+
+async Task<string> CreateFileAsync(string dirName, string fileName, string content)
+{
+    var filePath = Path.Combine(dirName, fileName);
     if (File.Exists(filePath))
     {
-        var fileContent = await File.ReadAllBytesAsync(filePath);
-        return (httpStatusCodes[200], fileContent);
+        return httpStatusCodes[201];
     }
-    return (httpStatusCodes[404], []);
+    await File.WriteAllTextAsync(filePath, content);
+    return httpStatusCodes[201];
+}
+
+string GetFilesDir(string[] args)
+{
+    var dirName = "/tmp";
+    if (args.Length < 2 || args[0] != "--directory") return dirName;
+
+    dirName = args[1];
+    if (!Directory.Exists(dirName))
+    {
+        throw new ArgumentException($"Directory {dirName} does not exist");
+    }
+    return dirName;
 }
